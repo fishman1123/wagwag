9add2bdd76cb945d262ff73b0b93e44b6eb1a6c0
jiyounglee <skfnxh1021@naver.com>
2024-08-16 11:00:27 +0900

    type : [#feature] 로그인 구현 완료

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { M } from 'vite/dist/node/types.d-aGj9QkWt';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,8 +7,40 @@ export default {
     "./public/**/*.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#080808',
+        secondary: '#57F98E',
+      },
+      fontFamily: {
+        sans: ['Pretendard', 'sans-serif'],
+      },
+      screens: {
+        sm: '314px',
+        md: '416px',
+        lg: '',
+        xl: '',
+        '2xl': '',
+      },
+      backgroundImage: {
+        setting: 'url("./assets/settingBg.png")'
+      },
+      logoImage: {
+        wagwaglogo: 'url("./assets/wagwaglogo.png")'
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function({ addBase }) {
+      addBase({
+        'html, body': {
+          'overflow-x': 'hidden',
+          // 'overflow-y': 'hidden',
+          // 'height': '100%',
+          // 'margin': '0',
+        },
+      });
+    },
+  ],
 }
 
